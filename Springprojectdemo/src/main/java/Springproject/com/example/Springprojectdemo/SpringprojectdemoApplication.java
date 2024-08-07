9bd8311e1863c10ae705da7068074a40007c3c79
Abhishek pal <Abhishekpal94244@gmail.com>
2024-08-07 18:15:10 +0530

    add

---
+++ Springprojectdemo/src/main/java/Springproject/com/example/Springprojectdemo/SpringprojectdemoApplication.java
@@ -0,0 +1,13 @@
+package Springproject.com.example.Springprojectdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectdemoApplication.class, args);
+	}
+
+}
---
+++ Springprojectdemo/src/test/java/Springproject/com/example/Springprojectdemo/SpringprojectdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Springproject.com.example.Springprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
